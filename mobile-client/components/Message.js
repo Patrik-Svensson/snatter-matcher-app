c7c910ea3cc0ac07334ff57016378514e8679a4e
Patrik Svensson <patriksvensson93@hotmail.com>
2021-05-18 23:44:35 +0200

    added chat input in mobile app

--- mobile-client/components/Message.js
+++ mobile-client/components/Message.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Text, View, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import Navbar from "./Navbar";
 import { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
@@ -7,6 +13,7 @@ const ENDPOINT = "http://localhost:3001";
 
 function Message() {
   [messages, setMessages] = useState([]);
+  [messageInput, setMessageInput] = useState("");
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
@@ -26,12 +33,27 @@ function Message() {
       style={{
         flex: 1,
         alignSelf: "stretch",
+        justifyContent: "center",
       }}
     >
       <FlatList
         data={mappedList}
         renderItem={({ item }) => <Text>{item.key}</Text>}
       />
+      <TextInput
+        style={{ marginLeft: 20, borderWidth: 1, width: 300 }}
+        value={messageInput}
+        onChangeText={setMessageInput}
+      />
+      <TouchableOpacity
+        style={{ marginLeft: 20 }}
+        onPress={() => {
+          const socket = socketIOClient(ENDPOINT);
+          socket.emit("chat message", messageInput);
+        }}
+      >
+        <Text>Skicka</Text>
+      </TouchableOpacity>
     </View>
   );
 }
